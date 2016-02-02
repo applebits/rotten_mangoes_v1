8c1ab4fb11c3af1bc1aee13d7c51ea150b55eafc
applebits <joeross.ca@gmail.com>
2016-02-02 23:39:55 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoesV1::Application.config.secret_key_base = 'aeb23fd498f2c2e99fbdbfcd2dcbf1b37f6ee251a3101b5d0eb6ce920a482ddc8d5e955b6f2dbc2ee8acecede52ffa2aad51c45dc7b5262d19309bc1b0037191'
